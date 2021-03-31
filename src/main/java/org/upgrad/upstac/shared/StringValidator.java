352d670d65aeb683513a63083ab78e74bf5f15a2
rrohitssharma009 <rrohitssharma009@gmail.com>
2021-03-31 17:28:04 +0530

    Initial commit

---
+++ src/main/java/org/upgrad/upstac/shared/StringValidator.java
@@ -0,0 +1,18 @@
+package org.upgrad.upstac.shared;
+
+public class StringValidator {
+
+   public static boolean isNotEmptyOrNull(String str) {
+       return null != str && !str.isEmpty();
+   }
+
+    public static boolean isNotEmptyOrNull(Object str) {
+        return null != str;
+    }
+    public static boolean isNotEmptyOrNull(Integer str) {
+        return null != str && str != 0;
+    }
+
+
+
+}
